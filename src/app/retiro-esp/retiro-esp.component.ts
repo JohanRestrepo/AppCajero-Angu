5439398b63a6cd8474ba493a80578dfcc6b6f0c2
JohanRestrepo <esnic1994@gmail.com>
2020-11-07 00:32:35 -0500

    sta listo el retiro especifico

--- src/app/retiro-esp/retiro-esp.component.ts
+++ src/app/retiro-esp/retiro-esp.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../usuario.service';
+import { Usuario } from '../usuarios';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-retiro-esp',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./retiro-esp.component.css']
 })
 export class RetiroEspComponent implements OnInit {
-
-  constructor() { }
+  User: Usuario;
+  noValue= false;
+  noNumero= false;
+  constructor(private UserService: UsuarioService, private router: Router) { }
 
   ngOnInit(): void {
+    this.getUsuarios(); 
+  }
+
+  Retiro(value): void{
+    if (value) {
+      if(isNaN(value)){
+        this.noNumero= true;
+        this.noValue= false;
+      }
+      else{
+        if(value <= this.User.saldo){
+          this.UserService.RetirarSaldo(value);
+          this.router.navigateByUrl('/ResEx');
+        }
+        else{
+          this.router.navigateByUrl('/ResFail');
+        }
+      }
+    }
+    else{
+      this.noValue= true;
+    }
+  }
+
+  getUsuarios(): void {
+    this.User = this.UserService.getUsuarios();
   }
 
 }
